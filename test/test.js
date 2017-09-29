eb0c9d9326cc61f971a9c3c40a5951db7f523550
bryan <bryan.sandras@hotmail.fr>
2017-09-29 15:44:09 +0200

    teste 43 coord valides

--- test/test.js
+++ test/test.js
@@ -6,4 +6,20 @@ LyngkTestCase.prototype.testStory1 = function () {
     var coordinates = new Lyngk.Coordinates('A',1);
 
     assertFalse(coordinates.is_valid());
+};
+
+LyngkTestCase.prototype.testStory2 = function () {
+    var colones = ['A','B','C','D','E','F','G','H','I'];
+    var lignes = ['1','2','3','4','5','6','7','8','9'];
+    var count=0;
+    for(var i in colones) {
+        for(var j in lignes) {
+            var coordinates = new Lyngk.Coordinates(colones[i],lignes[j]);
+            if(coordinates.is_valid() == true){
+                count++;
+            }
+        }
+    }
+    console.log(count);
+    assertEquals(count, 43);
 }
